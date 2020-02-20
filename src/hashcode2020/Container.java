5e7d0ebb99b884a76546e85126bd1a07023c52e8
Brais <Brais@LAPTOP-9A3CP77F>
2020-02-20 20:37:03 +0100

    Container añadido

---
+++ src/hashcode2020/Container.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode2020;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Team UwU
+ */
+public class Container {
+    
+    private ArrayList<Library> listaBibliotecas;
+    private ArrayList<Book> listaLibrosTotales;
+    private int ndays;
+    private ArrayList<Boolean> listaLibrosEnviados;
+    
+
+    public Container(ArrayList<Library> listaBibliotecas, ArrayList<Book> listaLibrosTotales, int ndays) {
+        this.listaBibliotecas = listaBibliotecas;
+        this.listaLibrosTotales = listaLibrosTotales;
+        this.ndays = ndays;
+        this.listaLibrosEnviados = new ArrayList<Boolean>(listaLibrosTotales.size());
+        
+        for(int i = 0; i< listaLibrosTotales.size();i++){
+            listaLibrosEnviados.add(Boolean.FALSE);
+        }
+    }
+    
+    public void ordenarBibliotecas(){
+        ArrayList<Library> aux = new ArrayList<Library>();
+        
+    }
+    
+     private static void quickSort(ArrayList<Library> arr, int start, int end){
+ 
+        int partition = partition(arr, start, end);
+ 
+        if(partition-1>start) {
+            quickSort(arr, start, partition - 1);
+        }
+        if(partition+1<end) {
+            quickSort(arr, partition + 1, end);
+        }
+    }
+ 
+    private static int partition(ArrayList<Library> arr, int start, int end){
+        Library pivot = arr.get(end);
+ 
+        for(int i=start; i<end; i++){
+            if(arr.get(i).maxPoints<pivot.maxPoints){
+                Library temp= arr.get(start);
+                arr.set(start, arr.get(i));
+                arr.set(i,temp);
+                start++;
+            }
+        }
+ 
+        Library temp = arr.get(start);
+        arr.set(start,  pivot);
+        arr.set(end, temp);
+ 
+        return start;
+    }
+    
+}
--- src/hashcodepizzatest/HashCodePizzaTest.java
+++ src/hashcodepizzatest/HashCodePizzaTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hashcode2020;
+package hashcodepizzatest;
 
 import java.io.*;
 import java.util.*;
