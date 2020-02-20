495c1ddba3d8ac856d90ad7ac8c4954e11ed4660
Brais <Brais@LAPTOP-9A3CP77F>
2020-02-20 18:06:52 +0100

    Listos para el reto!

--- src/hashcode2020/HashCode2020.java
+++ src/hashcode2020/HashCode2020.java
@@ -1,21 +1,54 @@
-/*
+/* ESPAÑA
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package hashcode2020;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
- * @author cgmr2
+ * @author Brais
  */
 public class HashCode2020 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hola a mi gos!");
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FileWriter fw = null;
+        PrintWriter salida = null;
+
+        try {
+
+
+            File fichero = new File(args[0]);
+            File ficheroSalida = new File("Salida.out");
+
+            fw = new FileWriter(ficheroSalida);
+            salida = new PrintWriter(fw);
+            
+            
+            
+            Scanner entrada = new Scanner(fichero);
+
+            
+            
+            salida.print("texto de ejemplo \n");
+            
+            
+
+
+            fw.close();
+
+        } catch (Exception exc) {
+            System.err.println(exc);
+        } finally {
+
+        }
+
     }
-    
 }
