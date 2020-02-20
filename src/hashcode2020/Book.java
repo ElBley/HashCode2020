b32c9748ef159e5b1b582fa0c43634aa0e7806b2
Cesar Marquez <cmrodriguez17@esei.uvigo.es>
2020-02-20 19:23:15 +0100

    Clases

---
+++ src/hashcode2020/Book.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode2020;
+
+/**
+ *
+ * @author cgmr2
+ */
+public class Book {
+    private int id;
+    private int score;
+
+    public Book(int id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    
+}
