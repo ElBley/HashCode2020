1cfb9a3f10c352b92127c41e9c6c44de9855a5b3
Cesar Marquez <cmrodriguez17@esei.uvigo.es>
2020-02-20 16:34:45 +0100

    Fixed

--- src/hashcode2020/HashCode2020.java
+++ src/hashcode2020/HashCode2020.java
@@ -15,7 +15,7 @@ public class HashCode2020 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.Out.Println("Hola amigos");
+        System.out.println("Hola amigos");
     }
     
 }
